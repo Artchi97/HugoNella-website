86caaea766c4760aaf842207f01d66d6d55c445f
Artchi97 <artur.wojciechowski97@gmail.com>
2025-01-02 19:37:11 +0100

    new feed text

--- frontend/src/feed.js
+++ frontend/src/feed.js
@@ -1,13 +1,14 @@
 export const feedData = [
   {
     id: 1,
-    date: "1.10.2024",
-    feedTitle: "Ana będzie mamą!",
+    date: "30.12.2024",
+    feedTitle: "Krajowa Wystawa Psów Rasowych - Świebodzice",
     description:
-      "W październiku spodziewamy się czarnych i czekoladowych szczeniąt.",
+      "Tradycyjnie na koniec roku pojechaliśmy na Krajową Wystawę Psów Rasowych do Świebodzic, organizowanej przez oddział Wałbrzych 😊 <br /> Sędzia: Joanna Dąbrowska <br />🥇Passion of my life INDIA/ Frida- klasa championów, ocena doskonała, lokata 1 CWC- Zwycięzca Rasy/ BOB😁🏆 <br /> 🥇 SWEET TEDDY HugoNella- klasa pośrednia, ocena doskonała, lokata 1 CWC- Zwycięzca Płci Przeciwnej/ BOS 😁🏆 <br /> 🥇RIVER HugoNella- klasa młodzieży, ocena doskonała, lokata 1- Zwycięzca Młodzieży oraz Najlepszy Junior w Rasie - tym samym Piesek Pawła rozpoczyna starania o Młodzieżowego Championa  Polski 😁🏆",
     photo:
-      "https://res.cloudinary.com/dueuqywlb/image/upload/v1728977987/LitterX-main_gciyud.jpg",
-    feedTitleEng: "Ana is going to be a mom!",
-    descriptionEng: "We are expecting black and chocolate puppies in October.",
+      "https://res.cloudinary.com/dueuqywlb/image/upload/v1735842727/0788ac4e-0666-4b60-aadd-bc946ba89fb1_xnwh09.webp",
+    feedTitleEng: "National Kennel Club Dog Show - Świebodzice",
+    descriptionEng:
+      "Traditionally, at the end of the year, we went to the National Kennel Club Dog Show in Świebodzice, organized by the Wałbrzych branch 😊 <br /> Judge: Joanna Dąbrowska <br /> 🥇 Passion of my life INDIA/ Frida – champion class, excellent rating, 1st place CWC – Best of Breed/BOB 😁🏆 <br /> 🥇 SWEET TEDDY HugoNella – intermediate class, excellent rating, 1st place CWC – Best Opposite Sex/BOS 😁🏆 <br /> 🥇 RIVER HugoNella – junior class, excellent rating, 1st place – Junior Winner and Best Junior in Breed – thus, Paweł's dog starts the journey towards the Polish Junior Champion title 😁🏆",
   },
 ];
